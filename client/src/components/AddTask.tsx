ebec62c7f69bfe3f19e51fa4781d74d723ba4b45
Jomark Pangan <jomarkrazonpangan@gmail.com>
2022-01-12 17:10:00 +0800

    created add task basic function

--- client/src/components/AddTask.tsx
+++ client/src/components/AddTask.tsx
@@ -1,28 +1,100 @@
-import { Box, Card, Typography } from "@mui/material";
+import { Box, Button, Card, TextField, Typography } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
+import { useState } from "react";
+import { Task } from "../types";
+import { v4 } from "uuid";
 
 const AddTask: React.FC = () => {
+  const [openAddTask, setOpenAddTask] = useState(false);
+
+  const [task, setTask] = useState<Task>({
+    id: v4(),
+    name: "",
+    duration: "0",
+    elapsed: "",
+    isCompleted: false,
+    icon: <AddIcon />,
+  });
+
+  const handleAddTask = (e: React.FormEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    console.log(task);
+
+    setOpenAddTask(false);
+  };
+
   return (
     <Box sx={{ mx: "auto" }}>
-      <Card
-        sx={{
-          m: "1rem auto",
-          width: "258px",
-          height: "90px",
-          background: "white",
-          borderRadius: "0.5rem",
-          border: "2px dashed gray",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <AddIcon sx={{ color: "text.secondary" }} />
-        <Typography variant="body2" color="textSecondary">
-          Add Task
-        </Typography>
-      </Card>
+      {!openAddTask ? (
+        <Card
+          onClick={() => setOpenAddTask(!openAddTask)}
+          sx={{
+            m: "1rem auto",
+            width: "258px",
+            height: "90px",
+            background: "white",
+            borderRadius: "0.5rem",
+            border: "2px dashed gray",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "lightgrey",
+          }}
+        >
+          <AddIcon sx={{ color: "text.secondary" }} />
+          <Typography variant="body2" color="textSecondary">
+            Add Task
+          </Typography>
+        </Card>
+      ) : (
+        <Card
+          component="form"
+          onSubmit={handleAddTask}
+          sx={{
+            m: "1rem auto",
+            width: "258px",
+            height: "90px",
+            background: "white",
+            borderRadius: "0.5rem",
+            border: "2px dashed gray",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            p: "0.5rem",
+          }}
+        >
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <AddIcon sx={{ color: "text.secondary", fontSize: "2rem" }} />
+            <TextField
+              size="small"
+              placeholder="task name"
+              value={task.name}
+              onChange={(e) => setTask({ ...task, name: e.target.value })}
+              sx={{ width: "65%", ml: "1.5rem" }}
+              InputLabelProps={{ style: { height: "0.5rem" }, shrink: true }}
+              inputProps={{
+                style: { height: "0.5rem", fontSize: "0.9rem" },
+              }}
+            />
+          </Box>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <input
+              type="number"
+              min="0"
+              value={task.duration}
+              onChange={(e) => setTask({ ...task, duration: e.target.value })}
+              style={{ width: "50px", height: "30px", border: 0, textAlign: "center" }}
+            />
+            <Button size="small" onClick={() => setOpenAddTask(!openAddTask)}>
+              Cancel
+            </Button>
+            <Button size="small" type="submit">
+              Add
+            </Button>
+          </Box>
+        </Card>
+      )}
     </Box>
   );
 };
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -5,7 +5,9 @@ import { CacheProvider, EmotionCache } from "@emotion/react";
 import Head from "next/head";
 import theme from "../styles/theme";
 import Layout from "../components/Layout";
-
+import { Provider as ReduxProvider } from "react-redux";
+import store from "../redux/store";
+import "../styles/globals.css";
 // for serverside MUI
 const clientSideEmotionCache = createEmotionCache();
 interface MyAppProps extends AppProps {
@@ -21,14 +23,16 @@ function MyApp(props: MyAppProps) {
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <>
-        <ThemeProvider theme={theme}>
-          <CacheProvider value={emotionCache}>
-            <CssBaseline />
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </CacheProvider>
-        </ThemeProvider>
+        <ReduxProvider store={store}>
+          <ThemeProvider theme={theme}>
+            <CacheProvider value={emotionCache}>
+              <CssBaseline />
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            </CacheProvider>
+          </ThemeProvider>
+        </ReduxProvider>
       </>
     </>
   );
