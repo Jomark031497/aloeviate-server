c56ce4049b968347080fde55488780f290a75d13
Jomark Pangan <jomarkrazonpangan@gmail.com>
2022-01-25 19:36:52 +0800

    getting the active task

--- client/src/components/Timer.tsx
+++ client/src/components/Timer.tsx
@@ -2,23 +2,48 @@ import { Box, Typography, IconButton } from "@mui/material";
 import PlayIcon from "@mui/icons-material/PlayCircle";
 import PauseIcon from "@mui/icons-material/PauseCircle";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import { Task } from "../types";
+import { minsToTimeFormat } from "../lib/timeFormatter";
 
 const Timer: React.FC = () => {
-  const [playTimer, setPlayTimer] = useState(false);
+  const tasks = useSelector((state: RootState) => state.tasks);
+  const [currentTask, setCurrentTask] = useState<Task | null>(null);
 
+  useEffect(() => {
+    if (tasks.length) {
+      setCurrentTask(tasks[0]);
+    } else {
+      setCurrentTask(null);
+    }
+  }, [tasks]);
+
+  const [playTimer, setPlayTimer] = useState(false);
   const toggleTimer = () => setPlayTimer((prev) => !prev);
 
   return (
     <Box id="timer-container" sx={{ my: "1rem" }}>
-      <>
-        <Typography variant="h2" align="center">
-          00:00
-        </Typography>
-        <Typography variant="h6" align="center">
-          No Task
-        </Typography>
-      </>
+      {currentTask ? (
+        <>
+          <Typography variant="h2" align="center">
+            {minsToTimeFormat(currentTask.duration)}
+          </Typography>
+          <Typography variant="h6" align="center">
+            {currentTask.name}
+          </Typography>
+        </>
+      ) : (
+        <>
+          <Typography variant="h2" align="center">
+            00:00
+          </Typography>
+          <Typography variant="h6" align="center">
+            No Task
+          </Typography>
+        </>
+      )}
 
       <Box id="timer-action-button" sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
         <Typography variant="h6">-5</Typography>
