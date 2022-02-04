6c3631baa81d2e087a7d5d619bdc33ed01eb57f0
Jomark Pangan <jomarkrazonpangan@gmail.com>
2022-02-05 07:46:34 +0800

    created task endpoints

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -17,3 +17,55 @@ export const add = async (req: Request, res: Response) => {
     return res.status(500).json({ error: "something went wrong" });
   }
 };
+
+export const getTasks = async (req: Request, res: Response) => {
+  try {
+    const tasks = await Task.find().where("user").equals(req.user);
+
+    return res.status(200).json(tasks);
+  } catch (error) {
+    return res.status(500).json({ error: "something went wrong" });
+  }
+};
+
+export const getTask = async (req: Request, res: Response) => {
+  const { id: _id } = req.params;
+
+  try {
+    const task = await Task.findById(_id);
+    if (!task) return res.status(404).json({ error: "task not found" });
+    return res.status(200).json(task);
+  } catch (error) {
+    return res.status(500).json({ error: "something went wrong" });
+  }
+};
+
+export const updateTask = async (req: Request, res: Response) => {
+  const { id: _id } = req.params;
+  const { name, duration } = req.body;
+  try {
+    let task = await Task.findById(_id);
+    if (!task) return res.status(404).json({ error: "task not found" });
+    task.name = name;
+    task.duration = duration;
+
+    task = await task.save();
+
+    return res.json(task);
+  } catch (error) {
+    return res.status(500).json({ error: "something went wrong" });
+  }
+};
+
+export const deleteTask = async (req: Request, res: Response) => {
+  const { id: _id } = req.params;
+
+  try {
+    const task = await Task.findById(_id);
+    if (!task) return res.status(404).json({ error: "task not found" });
+    await task.remove();
+    return res.status(200).json({ message: "task removed" });
+  } catch (error) {
+    return res.status(500).json({ error: "something went wrong" });
+  }
+};
--- src/routes/api/task.routes.ts
+++ src/routes/api/task.routes.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import { add } from "../../controllers/task.controller";
+import { add, deleteTask, getTask, getTasks, updateTask } from "../../controllers/task.controller";
 import requireAuth from "../../middlewares/requireAuth";
 
 const router = Router();
 
 router.post("/", requireAuth, add);
+
+router.get("/", requireAuth, getTasks);
+
+router.get("/:id", requireAuth, getTask);
+
+router.put("/:id", requireAuth, updateTask);
+
+router.delete("/:id", requireAuth, deleteTask);
 
 export default router;
