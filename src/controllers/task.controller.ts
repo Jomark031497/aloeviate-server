21e71bbd83c3c2c481f54052b59a6d39788b1118
Jomark Pangan <jomarkrazonpangan@gmail.com>
2022-02-06 09:08:15 +0800

    changed deleteTask endpoint response to the deleted item

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -84,7 +84,7 @@ export const deleteTask = async (req: Request, res: Response) => {
     const task = await Task.findById(_id);
     if (!task) return res.status(404).json({ error: "task not found" });
     await task.remove();
-    return res.status(200).json({ message: "task removed" });
+    return res.status(200).json(task);
   } catch (error) {
     return res.status(500).json({ error: "something went wrong" });
   }
